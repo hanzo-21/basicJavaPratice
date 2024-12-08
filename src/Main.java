6763f613d3a6e41de550843699100f643c780ce0
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-08 23:59:03 +0545

    added function to find average of elements in an array

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,35 @@ class Main {
         input.close();
     }
 
+    public static void findAverageOfArray(){
+
+        // create object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // create double array of size 5
+        double[] numbers = new double[5];
+
+        // get input value for the array
+        for (int index = 0; index < numbers.length; ++index) {
+            numbers[index] = input.nextDouble();
+        }
+
+        double sum = 0.0;
+
+        // add all array elements
+        for (int index = 0 ; index < numbers.length;index++) {
+            // find the sum
+            sum = sum + numbers[index];
+        }
+
+        // find the average
+        double average = sum/ numbers.length;
+        System.out.println(average);
+
+        input.close();
+    }
+
     public static void main(String[] args) {
-        Main.primeNumberChange4th();
+        Main.findAverageOfArray();
     }
 }
