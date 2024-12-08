2acbead1bb9f0fefa4f02dc4fe324992fb63594c
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-09 00:08:35 +0545

    smallest in an array

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,26 @@ class Main {
         System.out.println(average);
     }
 
+    public static void smallestInArray(){
+
+        // an array of numbers
+        int[] numbers = { 55, 64, 75, 80, 65 };
+
+        // assign the first value of the array to the smallest variable
+        int smallest = numbers[0];
+
+        // iterate each element of the array
+        // compare each element with the smallest
+        for(int num : numbers){
+            if(smallest > num){
+                smallest = num;
+            }
+        }
+
+        System.out.println(smallest);
+    }
+
     public static void main(String[] args) {
-        Main.findAverageForEachLoop();
+        Main.smallestInArray();
     }
 }
