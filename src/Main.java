7c660edd5a7505e06961d9e2bc0e2a9c7c9cb9f0
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-09 15:43:37 +0545

    update java methods 8

--- src/Main.java
+++ src/Main.java
@@ -124,12 +124,33 @@ class Main {
         int result = number * number * number;
         System.out.println(result);
     }
+
+    // method that adds two double number
+    void addNumbers(double n1, double n2) {
+        // add two numbers and print the result
+        double result = n1+n2;
+        System.out.println(result);
+    }
+
+
+
     public static void main(String[] args) {
+
+        // take input value
+        Scanner input = new Scanner(System.in);
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
 
         // creating object of Main class
         Main obj = new Main();
 
-        // call findCube using the object
-        obj.findCube();
+                // call the method with arguments number1 and number2
+        obj.addNumbers(number1,number2);
+
+        input.close();
+
+
+
+
     }
 }
