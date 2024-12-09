461b1b37a12e7756a93631fb81e4ac44c5caa6eb
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-09 19:07:10 +0545

    update java methods 20

--- src/Main.java
+++ src/Main.java
@@ -277,12 +277,27 @@ class Main {
         input.close();
     }
 
+    void printTheLargest(){
+
+        // get input value
+        Scanner input = new Scanner(System.in);
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        // compare number1 with number2
+        // if both numbers are equal, mark number1 as larger
+        int larger = (number1 > number2)?number1:number2;
+        System.out.println(larger);
+
+        input.close();
+    }
+
     public static void main(String[] args) {
 
         Main obj = new Main();
 
 
-        obj.nestedLoop();
+        obj.printTheLargest();
 
 
 
