e9977873819ae4e3d8e278d3e7ba428d9dd27e34
aayush rana magar <realnaramagaraayush@gmail.com>
2025-03-25 08:02:43 +0545

    added inveese stars

--- src/Main.java
+++ src/Main.java
@@ -381,12 +381,21 @@ class Main {
         }
     }
 
+    void inversestarTrangel(int n){
+        for (int i = 0 ;i < n ; i ++){
+            for (int j = i ;j<=n;j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Main obj = new Main();
 
         
-        obj.starTrangel(10);
+        obj.inversestarTrangel(10);
 
 
 
