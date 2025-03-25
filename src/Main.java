fc1424216610326cdef4c97d3d9b59453f8b073f
aayush rana magar <realnaramagaraayush@gmail.com>
2025-03-25 07:55:28 +0545

    added stars

--- src/Main.java
+++ src/Main.java
@@ -372,12 +372,21 @@ class Main {
         }
     }
 
+    void starTrangel(int n){
+        for (int i = 0 ;i < n ; i ++){
+            for (int j = 0 ;j<=i;j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Main obj = new Main();
 
         
-        obj.factorial(10);
+        obj.starTrangel(10);
 
 
 
